fd06db2881f8bbdae5d15e8f4492ec24f3adee8b
chiragahir20 <chiragchovatiya2005@gmail.com>
2025-07-17 21:53:37 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://mrchovatiya20:FoK2yIkeuAB8kJrH@cluster0.duylgzg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect('mongodb+srv://<user.name>:<password>@<cluster.name>.duylgzg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
