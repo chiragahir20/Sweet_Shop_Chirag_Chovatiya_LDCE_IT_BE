50b04605b564b6950d2fa0f6e64d9c6c622989ff
chiragahir20 <chiragchovatiya2005@gmail.com>
2025-07-17 19:36:02 +0530

    Implement Restock sweet feature

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
-const sweetRoutes = require('./addSweet/addSweet.routes');
-const deleteSweetRoutes = require('./deleteSweet/deleteSweet.routes');
+const sweetRoutes = require('./addSweet/addSweet.route');
+const deleteSweetRoutes = require('./deleteSweet/deleteSweet.route');
+const restockSweetRoutes = require('./restockSweet/restockSweet.route');
 
 app.use(express.json());
 app.use('/', sweetRoutes);
 app.use('/', deleteSweetRoutes);
+app.use('/', restockSweetRoutes);
 
 module.exports = app;
